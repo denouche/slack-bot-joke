78b19d424eada40e07a009718e17c2ddd6bdd97b
antoine.leveugle <antoine.leveugle@gmail.com>
2016-10-02 19:25:26 +0200

    fix(pony): now ponies are running running

--- app/pony/pony.js
+++ app/pony/pony.js
@@ -1,15 +1,16 @@
 let htmlparser = require('htmlparser2'),
 	request = require('request'),
-	q = require('q');
+	q = require('q'),
+	_ = require('lodash'),
+	entities = require('entities');
 
-const PONY_API_URL = "https://derpibooru.org/";
+const PONY_API_URL = "https://derpibooru.org";
 const HEADERS = {
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Accept-Encoding': 'deflate, gzip',
 	'Accept-Language': 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Authority': 'https://derpibooru.org',
 	'Cache-Control': 'no-cache',
-	'Connection': 'keep-alive',
-	'Host': 'https://derpibooru.org',
 	'Pragma': 'no-cache',
 	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36'
 };
@@ -28,21 +29,25 @@ function getPonyPicFromId(id) {
 		gzip: true,
 		headers: HEADERS
 	}, (error, response, body) => {
-		console.info('response "derpibooru"', response.statusCode);
+		console.info('response "derpibooru" image', response.statusCode);
 		if(!error && response.statusCode == 200) {
 			let domUtils = require('htmlparser2').DomUtils;
 			let handler = new htmlparser.DomHandler((err, dom) => {
-				let phrase = domUtils.findAll((elem) => {
-					if(elem.attribs && elem.attribs.id === 'image-display') {
+				let img = domUtils.findAll((elem) => {
+					if(elem.attribs 
+						&& elem.attribs.class 
+						&& _.split(elem.attribs.class, /\s+/).indexOf('image-show') !== -1) {
 						return true;
 					}
 					return false;
 				}, dom);
 
-				if(phrase.length > 0
-					&& phrase[0].children.length > 0
-					&& phrase[0].children[0].data) {
-					deferred.resolve(phrase[0].children[0].data.trim());
+				if(img.length > 0
+					&& img[0].attribs
+					&& img[0].attribs['data-uris']) {
+					var urls = entities.decodeHTML(img[0].attribs['data-uris']),
+						urlJson = JSON.parse(urls);
+					deferred.resolve(`https:${urlJson.small}`);
 				}
 				else {
 					deferred.reject();
@@ -62,11 +67,12 @@ function getRandomPonyId() {
 	request({
 	    url: `${PONY_API_URL}/search/index.json?q=safe%2Csolo&random_image=y`,
 	    method: 'GET',
-	    gzip: true
-	}, (error, response) => {
-		console.info('response "derpibooru"', response.statusCode);
+	    gzip: true,
+	    json: true
+	}, (error, response, body) => {
+		console.info('response "derpibooru" ID ', response.statusCode);
 	    if(!error && response.statusCode == 200) {
-	    	deferred.resolve(response.data);
+	    	deferred.resolve(body.id);
 	    }
 	    else {
 	    	deferred.reject();
