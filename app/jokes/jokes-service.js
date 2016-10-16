71fe38d70aaf237231b34fa43279a3538046fe67
Aurélien Allienne <aurelien.allienne@gmail.com>
2016-10-16 16:09:48 +0200

    feat(core): Refactor using es6 module

---
+++ app/jokes/jokes-service.js
@@ -0,0 +1,25 @@
+let Service = require('../Service'),
+
+    providers = [
+        require('./providers/humour-blague-service.js'),
+        require('./providers/marrez-vous-service.js'),
+        require('./providers/labanane-service.js')
+    ],
+    providersOption = {
+        maxLength: 600
+    };
+
+const MATCHING_WORS = ['blague', 'blagues'];
+
+class Joke extends Service {
+
+    getResponse() {
+        return providers[Math.floor(Math.random() * providers.length)].get(providersOption);
+    }
+
+    getMatchingWords() {
+        return MATCHING_WORS;
+    }
+}
+
+module.exports = Joke;
--- app/pony/pony.js
+++ app/pony/pony.js
@@ -1,8 +1,6 @@
-let htmlparser = require('htmlparser2'),
-	request = require('request'),
-	q = require('q'),
-	_ = require('lodash'),
-	entities = require('entities');
+let _ = require('lodash'),
+	entities = require('entities'),
+	Service = require('../Service');
 
 const PONY_API_URL = "https://derpibooru.org";
 const HEADERS = {
@@ -14,71 +12,45 @@ const HEADERS = {
 	'Pragma': 'no-cache',
 	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36'
 };
+const MATCHING_WORS = ['pony', 'ponies', 'poney', 'cheval', 'horse'];
 
-function getPonyPic() {
-	return getRandomPonyId().then((id) => {
-		return getPonyPicFromId(id);
-	});
+class Pony extends Service {
+
+	constructor() {
+		super('sendUrl');
+	}
+
+	matches(elem) {
+		return elem.attribs
+			&& elem.attribs.class
+			&& _.split(elem.attribs.class, /\s+/).indexOf('image-show') !== -1;
+	}
+
+	hasData(data) {
+		return data.length > 0
+			&& data[0].attribs
+			&& data[0].attribs['data-uris'];
+	}
+
+	resolveData(data) {
+		var urls = entities.decodeHTML(data[0].attribs['data-uris']),
+			urlJson = JSON.parse(urls);
+		return `https:${urlJson.small}`;
+	}
+
+	getUrl() {
+		return 'https://derpibooru.org/images/random';
+	}
+
+	getHeaders() {
+		return HEADERS;
+	}
+
+	getMatchingWords() {
+		return MATCHING_WORS;
+	}
+
+
 }
 
-function getPonyPicFromId(id) {
-	let deferred = q.defer();
-	request({
-		url: `${PONY_API_URL}/${id}`,
-		method: 'GET',
-		gzip: true,
-		headers: HEADERS
-	}, (error, response, body) => {
-		console.info('response "derpibooru" image', response.statusCode);
-		if(!error && response.statusCode == 200) {
-			let domUtils = require('htmlparser2').DomUtils;
-			let handler = new htmlparser.DomHandler((err, dom) => {
-				let img = domUtils.findAll((elem) => {
-					if(elem.attribs 
-						&& elem.attribs.class 
-						&& _.split(elem.attribs.class, /\s+/).indexOf('image-show') !== -1) {
-						return true;
-					}
-					return false;
-				}, dom);
-
-				if(img.length > 0
-					&& img[0].attribs
-					&& img[0].attribs['data-uris']) {
-					var urls = entities.decodeHTML(img[0].attribs['data-uris']),
-						urlJson = JSON.parse(urls);
-					deferred.resolve(`https:${urlJson.small}`);
-				}
-				else {
-					deferred.reject();
-				}
-			});
-			new htmlparser.Parser(handler).parseComplete(body);
-		}
-		else {
-			deferred.reject();
-		}
-	});
-	return deferred.promise;
-}
-
-function getRandomPonyId() {
-	let deferred = q.defer();
-	request({
-	    url: `${PONY_API_URL}/search/index.json?q=safe%2Csolo&random_image=y`,
-	    method: 'GET',
-	    gzip: true,
-	    json: true
-	}, (error, response, body) => {
-		console.info('response "derpibooru" ID ', response.statusCode);
-	    if(!error && response.statusCode == 200) {
-	    	deferred.resolve(body.id);
-	    }
-	    else {
-	    	deferred.reject();
-	    }
-	});
-	return deferred.promise;
-}
-
-exports.get = getPonyPic;
+module.exports = Pony;
